482644470169425e18f7abfa173e402fac1c8e90
BerndDammer <Bernd.Dammer@web.de>
2015-11-10 19:49:34 +0100

    stepB_25_08_2014

--- README.md
+++
@@ -1,2 +0,0 @@
-# htpc
-setup for htpcs
---
+++ gs.sh
@@ -0,0 +1,8 @@
+tar -cf update_htpc.tar /home/knoppix/.kde/share/apps/kaffeine/scanfile.dvb
+tar -uf update_htpc.tar /home/knoppix/.kde/share/apps/kaffeine/sqlite.db
+tar -uf update_htpc.tar /home/knoppix/Desktop/*.desktop
+tar -uf update_htpc.tar /home/knoppix/Desktop/*.txt
+tar -uf update_htpc.tar /home/knoppix/Desktop/*.jpg
+
+tar -uf update_htpc.tar /mnt-system/KNOPPIX/knoppix.sh
+
